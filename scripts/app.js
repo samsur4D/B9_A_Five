9d122738970d11c470b7a7ae91eeee3f73073cdd
samsur4D <patarehan14600@gmail.com>
2024-02-19 10:54:36 +0600

    ok

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,10 @@
 // create first look 
 function firstLook(){
      const firstLook = document.getElementById('first-look');
-     firstLook.classList.add('hidden')
+     firstLook.classList.add('hidden');
+     const secondLook = document.getElementById('second-look');
+    secondLook.classList.remove('hidden');
 }
 
 // show second look
-    const secondLook = document.getElementById('second-look');
-    secondLook.classList.remove('hidden');
+    
