cdcfb88a6eebe50afc857e7374eb4ffb937b6403
samsur4D <patarehan14600@gmail.com>
2024-02-21 00:48:23 +0600

    ok

--- scripts/app.js
+++ scripts/app.js
@@ -66,22 +66,46 @@ function  applyCoupon(){
 
 
    
-    let phnNumber =document.getElementById('number').value ;
+//     let phnNumber =document.getElementById('number').value.trim() ;
+// function nextButton(){
+//        let phnNumber = document.getElementById('number').value;
+//     console.log(phnNumber)
+     
+//     if (ticketPurchased < 1 && phnNumber ==="") {
+//         alert('Minimum 1 ticket purchase and must provide your phone number');
+//         return;
+//     }
+    
+//     document.getElementById('popup').classList.toggle('hidden')
+// }
+
+
+// setInterval(() => {
+//     let phnNumber = document.getElementById('number').value;
+// }, 1000);
+
+
+  
+
+   
+  
+let phnNumber =document.getElementById('number').value.trim() ;
 function nextButton(){
-    //  let phnNumber = document.getElementById('number').value;
+       let phnNumber = document.getElementById('number').value;
     console.log(phnNumber)
-    if(ticketPurchased < 1 && phnNumber === "" ){
-        alert('Minimum 1 ticket purchase and must provide your phone number');
-        return;
-    }
+      
+    
     document.getElementById('popup').classList.toggle('hidden')
 }
 
 
 setInterval(() => {
     let phnNumber = document.getElementById('number').value;
-    console.log(phnNumber)
+    if (ticketPurchased >= 1 && phnNumber !== "") {
+              document.querySelector('#nxtBtn').removeAttribute('disabled');
+             
+          }else if (ticketPurchased >= 1 && phnNumber === ""){
+            document.querySelector('#nxtBtn').setAttribute('disabled', true);
+            
+          }
 }, 1000);
-
-
-  
